f31a1057c2a84237d62805292219df22d9758285
adikgh <adiikgh@gmail.com>
2025-10-03 14:19:42 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,418 @@
+<? include 'config/core.php';
+
+	// header('location: /catalog/');
+
+	// site setting
+	$menu_name = 'home';
+	$site_set['visible'] = 'after';
+	$site_set['header_wh'] = true;
+	$site_set['plyr'] = true;
+	// $site_set['header_logo'] = 'cl';
+	$site_set['preload'] = true;
+?>
+<? include 'block/header.php'; ?>
+
+	<div class="bl1">
+		<!-- <div class="bl1_a lazy_bag" data-src="/assets/img/bag/8.png"></div> -->
+		<div class="bl1_a bl1_a1 lazy_bag" data-src="/assets/img/2662684.jpg"></div>
+
+		<div class="bl_c">
+			<div class="bl1_c">
+				<div class="bl1_offer">Откройте для себя <br> комфорт и лечение</div>
+				<div class="bl1_offer2">Санаторий Коктерек</div>
+				<div class="bl1_disc">Забронируйте свой идеальный отдых <br> в тёплых лечебных водах Сарыагаша</div>
+				<div class="form_c">
+					<div class="form_im"><div class="btn disb_zab">Окунитесь в целебные воды</div></div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+
+	<!--  -->
+	<div class="blj2" id="">
+		<div class="bl_c">
+			<div class="head_c">
+				<div class="head_v1 txt_c">
+					<h1>Целебные воды, лечебные <br> источники и комфортный отдых</h1>
+					<div class="head_vt" >и это все Коктерек</div>
+				</div>
+			</div>
+			<div class="blj2_c">
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/IMG_6049.PNG"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Лечебная вода</div>
+						</div>
+					</div>
+				</div>
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/close-up-doctor-holding-patient.jpg"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Более 20 видов лечения</div>
+						</div>
+					</div>
+				</div>
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Чистый воздух</div>
+						</div>
+					</div>
+				</div>
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Профессиональный персонал</div>
+						</div>
+					</div>
+				</div>
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Уютная атмосфера</div>
+						</div>
+					</div>
+				</div>
+				<div class="blj2_i">
+					<div class="blj2_ic">
+						<div class="blj2_img lazy_img" data-src="/assets/img/bag/Frame 12 656.jpg"></div>
+						<div class="blj2_in">
+							<div class="blj2_in2">Вкусный питание</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!--  -->
+	<div class="blj3" id="">
+		<div class="bl_c">
+			<div class="blj3_c">
+				<div class="head_c">
+					<div class="head_v1 txt_c">
+						<h1>Территория для <br> отдыха и лечение</h1>
+						<div class="head_vt" >объединяющая людей</div>
+					</div>
+				</div>
+				<div class="blj3_cn">
+
+					<div class="blj3_i">
+						<div class="blj3_it">01.</div>
+						<div class="blj3_ih">Удобное расположение</div>
+						<div class="blj3_ip">Санаторий находится всего в 2 часа от Шымкента и легко доступен через удобные транспортные пути.</div>
+					</div>
+					<div class="blj3_i">
+						<div class="blj3_it">02.</div>
+						<div class="blj3_ih">Уютные, современные номера</div>
+						<div class="blj3_ip">Каждый номер оборудован всем необходимым для комфортного проживания и эффективного лечения.</div>
+					</div>
+					<div class="blj3_i">
+						<div class="blj3_it">03.</div>
+						<div class="blj3_ih">Целебные воды и восстановление энергии</div>
+						<div class="blj3_ip">Наслаждайтесь целебными водами и восстанавливайте силы в окружении природы, наполняющей вас новой энергией.</div>
+					</div>
+					<div class="blj3_i">
+						<div class="blj3_it">04.</div>
+						<div class="blj3_ih">Тишина и уединение</div>
+						<div class="blj3_ip">Санаторий расположен вдали от шума города, обеспечивая полное уединение и отдых от городской суеты.</div>
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="blj3_r">
+				<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d94158.10208707159!2d70.30119834158488!3d42.482182884759055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38a89c26bc3bcecb%3A0x682d1672e1baba5f!2z0J7Qt9C00L7RgNC-0LLQuNGC0LXQu9GM0L3Ri9C5INC60L7QvNC_0LvQtdC60YEg0KLQsNC9INCd0YPRgNGL!5e0!3m2!1sru!2skz!4v1718671292043!5m2!1sru!2skz" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2117.009607938442!2d69.0592685456762!3d41.37617693936111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae857c8968d45b%3A0x4e3e0d461d6f4237!2z0JDRgdGL0Lsg0LHRg9C70LDQug!5e0!3m2!1sru!2skz!4v1738669241456!5m2!1sru!2skz" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+
+
+
+	<!--  -->
+	<!-- <div class="blj4" id="">
+		<div class="bl_c">
+			<div class="head_c">
+				<div class="head_v1 txt_c">
+					<h1>Вы найдете то, что <br> нужно именно вам</h1>
+				</div>
+			</div>
+			<div class="blj4_c">
+
+				<div class="blj4_i">
+					<div class="blj4_it"></div>
+					<div class="blj4_ih">Переночевать</div>
+					<div class="blj4_ip">остановиться на ночлег и с радостью провести время любуясь красивой природой</div>
+				</div>
+				<div class="blj4_i">
+					<div class="blj4_it"></div>
+					<div class="blj4_ih">Уединиться</div>
+					<div class="blj4_ip">провести время наедине с собой и близкими в тихом уголке нашеи страны</div>
+				</div>
+				<div class="blj4_i">
+					<div class="blj4_it"></div>
+					<div class="blj4_ih">Исследовать мир</div>
+					<div class="blj4_ip">получить яркие впечатления и эмоции от экскурсий и экстремального туризма</div>
+				</div>
+				
+			</div>
+		</div>
+	</div> -->
+
+	
+	<!--  -->
+	<!-- <div class="blj5" id="bl2">
+		<div class="bl_c">
+			<div class="blj5_rz">
+				<div class="blj5_rzr">
+					<div class="blj5_rzrc lazy_img" data-src="/assets/img/bag/close-up-doctor-holding-patient.jpg"></div>
+				</div>
+				<div class="blj5_rzll">
+					<div class="head_c">
+						<div class="head_v1">
+							<h3>ВРЕМЕНННАЯ АКЦИЯ</h3>
+							<div class="head_vt" >Это не розыгрыш!</div>
+						</div>
+					</div>
+					<div class="blj5_te">Абсолютно каждому не курящему гостю, забронировавшему проживание в наших гостевых домах на свободные даты в мае, первые сутки проживания предоставляются с оплатой по сердцу, то есть сколько вашей душе будет угодно</div>
+					<div class="blj5_b" >
+						<a class="btn" target="_blank" href="https://wa.me/<?=$site['whatsapp']?>">ПЕРЕЙТИ К БРОНИРОВАНИЮ</a>
+					</div>
+				</div>
+				<div class="blj5_rzlr">* Начиная со вторых суток оплата за проживание согласно предоставленным тарифам</div>
+			</div>
+		</div>
+	</div> -->
+
+
+
+
+	<!-- block 5 -->
+	<div class="blx3" id="treatment">
+        <div class="bl_c">
+            <div class="head_c">
+                <h1 class="txt_tu txt_c">Виды лечения</h1>
+            </div>
+            <div class="blx3_c">
+                
+                <? $sql = db::query("select * from sanatorium_lesh where type = 1 and rek = 1 ORDER BY number ASC limit 5"); ?>
+                <? while ($ana = mysqli_fetch_array($sql)): ?>
+                    <div class="bl5_i " href="#em-vanna.php" >
+                        <div class="bl5_ia lazy_bag" data-src="/assets/uploads/em/<?=$ana['img']?>"></div>
+                        <div class="bl5_ic">
+                            <div class="bl5_ict"><?=$ana['name']?></div>
+                        </div>
+                    </div>
+                <? endwhile ?>
+
+                <div class="bl5_i bl5_iclc" >
+					<div class="btn">Показать все</div>
+				</div>
+			</div>
+
+            <!-- <div class="bl5_b ">
+                <p>У нас есть подходящие процедуры для каждого гостя</p>
+                <a href="lechenie.php"><div class="btn"><span>Посмотреть все процедуры </span><i class="far fa-long-arrow-right"></i></div></a>
+            </div> -->
+
+        </div>
+    </div>
+
+
+
+
+	<div class="sana_bl2" id="numbers">
+		<div class="bl_c">
+			<div class="head_c">
+                <h1 class="txt_tu txt_c">Наш уютные номера</h1>
+            </div>
+            <div class="sana_bl2_c">
+
+                <? $sql = db::query("select * from sanatorium_number where arh is null ORDER BY number ASC "); ?>
+                <? while ($ana = mysqli_fetch_array($sql)): ?>
+                    <div class="bl5_i">
+                        <div class="bl5_ic">
+							<div class="bl5_icb">
+								<div class="bl5_icb2">
+									<div class="bl5_icn"><?=$ana['type_name']?></div>
+									<div class="bl5_ictp"><?=$ana['door_name']?></div>
+								</div>
+                                <div class="bl5_icbn">
+                                    <div class="bl5_ics">Цена за одного гостя:</div>
+                                    <div class="bl5_icp fr_price"><?=$ana['price']?></div>
+                                </div>
+                            </div>
+                        </div>
+						<div class="bl5_ia lazy_bag" data-src="/assets/uploads/number/<?=$ana['img']?>" href="#"></div>
+                    </div>
+                <? endwhile ?>
+
+				<div class="bl5_i">
+					<div class="bl5_ix1">Все для комфортного проживания</div>
+					<div class="bl5_ix2">
+						<div class="">Туалет и душ во всех номера</div>
+						<div class="">Вид из окна на гору</div>
+						<div class="">Питание: ALL - ВСЕ ВКЛЮЧЕНО</div>
+						<div class="">WI-FI ЕСТЬ В НОМЕРАХ</div>
+						<div class="">КОНДИЦИОНЕР</div>
+						<div class="">ТЕЛЕВИЗОР</div>
+						<div class="">ХОЛОДИЛЬНИК</div>
+					</div>
+					<div class="bl5_icbtn">
+						<div class="btn btn_bl2 price_clc">Прайс</div>
+						<div class="btn disb_zab">Забронировать</div>
+					</div>
+				</div>
+
+
+            </div>
+        </div>
+	</div>
+
+	
+	
+	<!--  -->
+	<div class="bl9" id="review">
+		<div class="bl_c">
+			<div class="head_c">
+                <h1 class="txt_tu txt_c">Отзывы наших посетителей</h1>
+            </div>
+			<div class="bl9_c">
+				<div class="swiper-container bl9_con">
+					<div class="swiper-wrapper">
+						
+						<? $sql = db::query("select * from `review`"); ?>
+						<? while ($ana = mysqli_fetch_array($sql)): ?>
+							<div class="swiper-slide bl9_i" >
+								<div class="bl9_iln">
+									<div class="bl9_iln2">
+										<div class="bl9_ilogo lazy_rev" data-src="/assets/uploads/reviews/<?=@$ana['logo']?>"></div>
+										<div class="bl9_ilc"><div class="bl9_ilname"><?=$ana['name']?></div><div class="bl9_ils"><?=t::w('source:').$ana['sn']?></div></div>
+									</div>
+								</div>
+								<p class="bl9_it"><?=$ana['txt']?></p>
+							</div>
+						<? endwhile ?>
+						
+					</div>
+				</div>
+				<div class="swiper-button-prev bl9_prev"><div class="btn btn_sqr"><i class="fal fa-chevron-left"></i></div></div>
+				<div class="swiper-button-next bl9_next"><div class="btn btn_sqr"><i class="fal fa-chevron-right"></i></div></div>
+			</div>
+			<div class="bl9_b" >
+				<p><?=t::w('bl9_of3')?></p>
+				<a target="_blank" href="https://wa.me/<?=$site['whatsapp']?>"><div class="btn"><?=t::w('Communication')?></div></a>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!--  -->
+	<div class="bl7" id="about">
+		<div class="bl7_rzla"></div>
+		<div class="bl_c">
+			<div class="bl7_rz">
+				<div class="bl7_rzl">
+					<div class="head_c">
+						<div class="head_v1">
+							<h3><?=t::w('bl7_of2')?></h3>
+							<div class="head_vt" >О нас</div>
+						</div>
+					</div>
+					<div class="bl7_c">
+						<div class="bl7_te">«Главная ценность нашей команды – здоровье, то есть санаторий для нас – это не бизнес, а стиль жизни!»</div>
+						
+						<? $sql = db::query("select * from `word_blocks` where type = 'bl7' and lang = '$lang' ORDER BY number ASC"); ?>
+						<? while($ana = mysqli_fetch_array($sql)): ?>
+							<div class="bl7_i" >
+								<i class="far fa-badge-check"></i>
+								<div class="bl7_ic"><?=$ana['txt1']?></div>
+							</div>
+						<? endwhile ?>
+
+					</div>
+					<div class="bl7_b" >
+						<p><?=t::w('bl7_of3')?></p>
+						<a class="btn btn_whatsapp" target="_blank" href="https://wa.me/<?=$site['whatsapp']?>"><i class="fab fa-whatsapp"></i><span><?=t::w('Write')?></span></a>
+					</div>
+				</div>
+				<div class="bl7_rzr">
+					<div class="bl7_rzrc" data-src="/assets/img/bag/close-up-doctor-holding-patient.jpg">
+						video
+                    	<!-- <div class="player_o7" data-plyr-provider="youtube" data-plyr-embed-id="nXsnu9kejgw" data-poster="/assets/img/bag/IMG_1444.webp"></div> -->
+						<script>
+							// const player_o7 = new Plyr(".player_o7", {
+							// 	fullscreen: {iosNative: true},
+							// 	controls: ['play-large'],
+							// 	// poster: '/assets/img/bag/IMG_1444.webp',
+							// });
+						</script>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+
+	<!-- block 5 -->
+	<div class="blx3" id="certificates">
+        <div class="bl_c">
+            <div class="head_c">
+                <h1 class="txt_tu txt_c">Наши сертификат</h1>
+            </div>
+			<div class="serct_dc">
+				<img src="" alt="" class="lazy_img" data-src="/assets/uploads/">
+				<!-- <img src="" alt="" class="lazy_img" data-src="/assets/uploads/Report (1)_page-0002.jpg"> -->
+				<!-- <img src="" alt="" class="lazy_img" data-src="/assets/uploads/Report (1)_page-0003.jpg"> -->
+			</div>
+        </div>
+    </div>
+
+
+
+	<!--  -->
+	<div class="bl11">
+		<div class="bl_c">
+			<div class="bl11_c">
+				<div class="bl11_l">
+					<div class="head_c"><h4><?=t::w('Frequently asked Questions')?></h4></div>
+					<p><?=t::w('bl11_disc')?></p>
+					<div class="bl11_btn"><div class="btn disb_zab"><span><?=t::w('submit your')?></span></div></div>
+					<div class="bl11_a lazy_bag" data-src="/assets/img/bag/doc2.png"></div>
+				</div>
+				<div class="bl11_r">
+					<div class='faq'>
+						<? $sql = db::query("select * from `word_blocks` where type = 'bl11' and lang = '$lang' ORDER BY number ASC"); ?>
+						<? while($ana = mysqli_fetch_array($sql)): ?>
+							<div class="faq-a">
+								<div class="faq-ah">
+									<div class="faq-arrow"><i class="fal fa-chevron-right"></i></div>
+									<h6 class="faq-heading"><?=$ana['txt1']?></h6>
+								</div>
+								<p class="faq-text"><?=$ana['txt2']?></p>
+							</div>
+						<? endwhile ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<? include 'block/footer.php'; ?>
